2550a46fabb6047976a28af991575b99c3d3f04b
slawekradzyminski <slawomir.radzyminski@gmail.com>
2025-10-28 15:49:38 +0100

    str18

---
+++ tests/ui/home.ui.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '../../fixtures/uiAuthFixture';
+import { HomePage } from '../../pages/homePage';
+
+test.describe('Home UI tests', () => {
+  test('should display authenticated admin user email', async ({ page, authenticatedUIAdmin }) => {
+    // given
+    const homePage = new HomePage(page);
+
+    // when
+
+    // then
+    await homePage.expectUserEmail(authenticatedUIAdmin.userData.email);
+  });
+
+});
