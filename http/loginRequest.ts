603ea05cc404c7bb4dbf1878f08e2e3966e20c24
Slawek <slawomir.radzyminski@gmail.com>
2025-11-24 10:32:04 +0100

    play01

---
+++ http/loginRequest.ts
@@ -0,0 +1,14 @@
+import { APIRequestContext, APIResponse } from "@playwright/test";
+import type { LoginDto } from "../types/auth";
+import { API_BASE_URL } from "../config/constants";
+
+const SIGNIN_ENDPOINT = '/users/signin';
+
+export const attemptLogin = async (request: APIRequestContext, loginData: LoginDto): Promise<APIResponse> => {
+  return await request.post(`${API_BASE_URL}${SIGNIN_ENDPOINT}`, {
+    data: loginData,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
