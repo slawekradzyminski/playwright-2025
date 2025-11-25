0385a7f1dc9c70965b8b80b1293af0924f68288c
Slawek <slawomir.radzyminski@gmail.com>
2025-11-25 11:59:27 +0100

    play14

--- config/constants.ts
+++ config/constants.ts
@@ -1,3 +1,4 @@
 export const API_BASE_URL = 'http://localhost:4001';
+export const FRONTEND_URL = 'http://localhost:8081';
 
 export const INVALID_TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
